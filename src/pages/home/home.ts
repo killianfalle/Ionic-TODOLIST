3ce368503b2e10b70a7d45d815843653e2ca922d
killianfalle <killianfalle7@gmail.com>
2018-08-31 19:17:15 +0800

    second

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,41 +19,62 @@ export class HomePage {
   }
 
   add() {
-    this.todos.push(this.todo);
-    this.todo = "";
-    let alert = this.alertController.create({
-    title: 'Success!',
-    buttons: ['Dismiss']
+    let addTodoAlert = this.alertController.create({
+        
+        message:"Successfully Added!",
+        buttons:[{
+            text:"OK"
+        }
+    ]
     });
-
-    alert.present();
-
+    addTodoAlert.present()
+    this.todos.push(this.todo);
+    this.todo = "";    
 }
 
-
 delete(item) {
+    let delTodoAlert = this.alertController.create({
+        
+        message:"Successfully Deleted!",
+        buttons:[{
+            text:"OK"
+        }
+    ]
+    });
+    delTodoAlert.present()
     var index = this.todos.indexOf(item, 0);
     if (index > -1) {
         this.todos.splice(index, 1);
-
-        let alert = this.alertController.create({
-        title: 'Successfully Deleted!',
-        buttons: ['Dismiss']
-        });
-
-            alert.present();
-        }
-}
-
- edit(item) {
-    var index = this.todos.indexOf(item, 0);
-        if (index > -1) {
-        this.todos.splice(index, 1);
-        this.todo = item;
     }
-        
-    
-}
 }
 
+edit(item){
+    var index = this.todos.indexOf(item,0)
+    let editTodoAlert = this.alertController.create({
+        title:"Edit Todo",
+        message:"Please Input!",
+        inputs:[
+        {
+            type:"text",
+            name:"editTodoInput",
+            placeholder:item,
+        }],
+        buttons:[
+        {
+        text:"Update",
+        handler:(inputData)=>{
+            let todoText;
+            todoText = inputData.editTodoInput;
+            this.todos.splice(index,1);
+            this.todos.push(todoText);    
+        }   
+        }],
+    });
+    editTodoAlert.present()
+    // if (index > -1){
+    //     this.todos.splice(index,1);
+    //     this.todo = item;
+    // }
+}
 
+}
